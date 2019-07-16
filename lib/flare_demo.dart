6a29ed4200dbc67632d54f22eaa6454a60ebed21
Son Chau <soncqb@gmail.com>
2019-07-16 11:21:24 -0500

    using smart flare package

--- lib/flare_demo.dart
+++ lib/flare_demo.dart
@@ -1,6 +1,5 @@
-import 'package:flare_tutorial/smart_flare_animation.dart';
 import 'package:flutter/material.dart';
-import 'package:flare_flutter/flare_actor.dart';
+import 'package:smart_flare/smart_flare.dart';
 
 class FlareDemo extends StatefulWidget {
   @override
@@ -8,15 +7,65 @@ class FlareDemo extends StatefulWidget {
 }
 
 class _FlareDemoState extends State<FlareDemo> {
-  bool isOpen = false;
 
   @override
   Widget build(BuildContext context) {
+    var animationWidth = 295.0;
+    var animationHeight = 251.0;
+    var animationWidthThirds = animationWidth / 3;
+    var halfAnimationHeight =animationHeight / 2;
+
+    var activeAreas = [
+
+      ActiveArea(
+          area: Rect.fromLTWH(0, 0,
+              animationWidthThirds,
+              halfAnimationHeight),
+          debugArea: true,
+          guardComingFrom: ['deactivate'],
+          animationName: 'camera_tapped'),
+
+      ActiveArea(
+          area: Rect.fromLTWH(
+              animationWidthThirds, 0,
+              animationWidthThirds,
+              halfAnimationHeight),
+          debugArea: true,
+          guardComingFrom: ['deactivate'],
+          animationName: 'pulse_tapped'),
+
+      ActiveArea(
+          area: Rect.fromLTWH(
+              animationWidthThirds * 2, 0,
+              animationWidthThirds,
+              halfAnimationHeight),
+          debugArea: true,
+          guardComingFrom: ['deactivate'],
+          animationName: 'image_tapped'),
+
+      ActiveArea(
+          area: Rect.fromLTWH(
+              0,
+              animationHeight/2,
+              animationWidth,
+              animationHeight/2),
+          debugArea: true,
+          animationsToCycle: ['activate', 'deactivate'],
+          onAreaTapped: () {
+            print('Button tapped!');
+          })
+    ];
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 102, 18, 222),
-      body: Align(
-          alignment: Alignment.bottomCenter,
-          child: SmartFlareAnimation()),
-    );
+        backgroundColor: Color.fromARGB(255, 102, 18, 222),
+        body: Align(
+            alignment: Alignment.bottomCenter,
+            child: SmartFlareActor(
+                width: animationWidth,
+                height: animationHeight,
+                filename: 'assets/button-animation.flr',
+                startingAnimation: 'deactivate',
+                activeAreas: activeAreas
+            )
+        ));
   }
 }
